365981f553fd146cbd566acefae40611c799e4f4
Madhulika <madhulika.chavva@gmail.com>
2024-10-26 17:29:23 -0700

    add missing files

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Menu, Moon, Search, Settings, Sun, User } from "lucide-react";
 import Link from "next/link";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
 import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
+import { useGetAuthUserQuery } from "@/state/api";
 import { signOut } from "aws-amplify/auth";
 import Image from "next/image";
-import { useAppDispatch, useAppSelector } from "@/app/redux";
-import { useGetAuthUserQuery } from "@/state/api";
 
 const Navbar = () => {
   const dispatch = useAppDispatch();
