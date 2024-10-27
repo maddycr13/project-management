c9cb13731d562da6dc7caabd5927405a8dd6fa78
Madhulika <madhulika.chavva@gmail.com>
2024-10-27 14:36:31 -0700

    fix build issues

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: 'null-loader',
-    });
-
-    return config;
-  },
-
   images: {
     remotePatterns: [
       {
